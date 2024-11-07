cd5db349b2c46fc96fb62860e4b43bcefc1b5376
matiasfelau <matiasfelau@gmail.com>
2024-11-06 23:20:41 -0300

    V1.4.4 Implementación de la autenticación en tiempo real

--- build/lib/sender/__init__.py
+++ build/lib/sender/__init__.py
@@ -3,6 +3,9 @@ import json
 import threading
 import uuid
 from enum import Enum
+import json
+import time
+import uuid
 
 import pika
 
@@ -66,10 +69,11 @@ def close_connection(connection):
         print(f'\nError in sender.close_rabbitmq_connection(): \n{str(e)}')
 
 
-def publish(connection, message, origin, destination, use_case, token, type, target, status):
+def publish(connection, message, origin, destination, use_case, token, type, target, status, user):
     """
     Envia un mensaje al módulo de destino.
     Convierte el mensaje automáticamente a un JSON.
+    :param user:
     :param status:
     :param target:
     :param token:
@@ -92,11 +96,12 @@ def publish(connection, message, origin, destination, use_case, token, type, tar
             'status': status,
             'token': token,
             'type': type,
-            'target': target
+            'target': target,
+            'user': user
         }
         data = json.dumps(body).encode('utf-8')
         channel = connection.channel()
-        channel.exchange_declare(exchange='core', exchange_type='direct')
+        channel.exchange_declare(exchange='core', exchange_type='direct', durable=True)
         channel.basic_publish(exchange='core', routing_key='core', body=data)
     except Exception as e:
         print(f'\nError in sender.publish(): \n{str(e)}')
@@ -245,3 +250,65 @@ class Types(Enum):
     JSON = 'json'
     ARRAY = 'array'
 
+class Authenticator:
+    """
+
+    """
+    def __init__(self, connection, channel, module):
+        """
+
+        :param connection:
+        :param channel:
+        """
+        self.corr_id = None
+        self.response = None
+        self.connection = connection
+        self.channel = channel
+        result = channel.queue_declare(queue=f'{module}.rpc', exclusive=True)
+        self.callback_queue = result.method.queue
+        channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self._on_response,
+            auto_ack=True
+        )
+
+    def _on_response(self, ch, method, props, body):
+        """
+
+        :param ch:
+        :param method:
+        :param props:
+        :param body:
+        :return:
+        """
+        if self.corr_id == props.correlation_id:
+            self.response = body.decode('utf-8')
+        else:
+            raise Exception('Authenticator bad response')
+
+    def authenticate(self, message, timeout_ms=3000):
+        """
+
+        :param message:
+        :param timeout_ms:
+        :return:
+        """
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='gestion_interna.rpc',
+            routing_key='gestion_interna.rpc',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id
+            ),
+            body=message
+        )
+        start_time = time.time()
+        timeout = timeout_ms / 1000
+        while self.response is None:
+            elapsed_time = time.time() - start_time
+            if elapsed_time > timeout:
+                raise Exception('Authenticator did not respond in time')
+            self.connection.process_data_events()
+        return self.response
